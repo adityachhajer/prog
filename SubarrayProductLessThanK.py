87e12045ec2dd99d59b7c47a10f73070286baf80
aditya chhajer <chhajeraditya@gmail.com>
2020-10-01 10:48:18 +0530

    Update SubarrayProductLessThanK.py

--- SubarrayProductLessThanK.py
+++ SubarrayProductLessThanK.py
@@ -1,31 +1,35 @@
+def fac(n):
+    if n<=1:
+        return n
+    return int(n*(n+1)/2)
+
+def get(s,nums,k):
+    for i in range(s,len(nums)):
+        if nums[i]<k:
+            return i
+    return None
+
 class Solution:
-    def solve(self, i):
-        if i == 0:
-            return 0
-        if i == 1:
-            return 1
-        c = 1
-        for j in range(2, i + 1):
-            c = c + j
-        return c
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        p = 1
-        l = []
-        ans = 0
-        for i in nums:
-            p = p * i
-            if p < k:
-                l.append(i)
+        if k<=1:
+            return 0
+        start=get(0,nums,k)
+        if start==None:
+            return 0
+        ans=0
+        product=nums[start]
+        for i in range(start+1,len(nums)):
+            if product*nums[i]<k:
+                product*=nums[i]
             else:
-                while l and p >= k:
-                    ans += len(l)
-                    x = l.pop(0)
-                    p = p / x
-                if len(l) == 0:
-                    p = i
-                if p < k:
-                    l.append(i)
-                else:
-                    p = 1
-        a = self.solve(len(l))
-        return ans + a
+                ans+=(i-start)
+                product*=nums[i]
+                product/=nums[start]
+                start+=1
+                while product>=k:
+                    product/=nums[start]
+                    start+=1
+                    ans+=(i-start)+1
+        else:
+            ans+=fac(i+1-start)
+        return ans
